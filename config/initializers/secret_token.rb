e22f1bb32f718de6b796f1d8b19cf4f927f2b94d
Daren Warburton <Daren@Darens-MacBook-Air.local>
2012-04-07 13:24:47 +0100

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blackdogproperty::Application.config.secret_token = 'fc8e705e3a9fc19447cd2ce7f47218c4bc2ce3c9c0830a89e6f2797acd46cf64c43f4d88c9d7e4d8e11674dcd25cbcf64992907652b60c0776e9d03914524a8f'
