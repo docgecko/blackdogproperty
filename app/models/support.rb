9296117c22e505a93f6b3b71150bc334cc3765b5
Daren Serra Dias Warburton <daren.sdw@gmail.com>
2012-04-30 21:42:35 +0100

    correcting image border error

--- app/models/support.rb
+++ app/models/support.rb
@@ -9,9 +9,9 @@ class Support
   # Validations
   validates_presence_of :name, :message => "Your name is required."
   validates_presence_of :email, :message => "Your email address is required."
-  validates_presence_of :interest_ids, :message => "You must select at least 1 interest."
-  validates_presence_of :content, :message => "You must provide details of your enquiry (Other information)."
   validates_format_of :email, with: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i, :message => "Your email address is invalid."
+  validates_presence_of :interest_ids, :message => "You must select at least one interest."
+  validates_presence_of :content, :message => "You must provide details of your enquiry (Other information)."
   
   # References
   belongs_to :source
