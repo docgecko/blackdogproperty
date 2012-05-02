f1ad5aa5baaa88f1e9eb66d94bc05d3cfe5fa025
Daren Serra Dias Warburton <daren.sdw@gmail.com>
2012-05-02 12:12:24 +0100

    Updated email sending to include production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Blackdogproperty::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
   
-  config.action_mailer.default_url_options = { :host => 'blackdogproperty.com' }
+  # config.action_mailer.default_url_options = { :host => 'blackdogproperty.com' }
 
   # Enable threaded mode
   # config.threadsafe!
--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,12 +1,10 @@
-if Rails.env.development? or Rails.env.test?
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    :user_name => "app4264979@heroku.com",
-    :password => "nwiu0joh",
-    :domain => "blackdogproperty.heroku.com",
-    :address => "smtp.sendgrid.net",
-    :port                 => 587,
-    :authentication       => "plain",
-    :enable_starttls_auto => true  
-  }
-end
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :user_name => "app4264979@heroku.com",
+  :password => "nwiu0joh",
+  :domain => "blackdogproperty.heroku.com",
+  :address => "smtp.sendgrid.net",
+  :port                 => 587,
+  :authentication       => "plain",
+  :enable_starttls_auto => true  
+}
