b95b96602e4b1de5a68263fc7e372e98923f5676
Daren Serra Dias Warburton <daren.sdw@gmail.com>
2012-05-02 16:19:36 +0100

    updated contact email to enquiries@blackdogproperty.com

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -3,7 +3,7 @@ class Notifier < ActionMailer::Base
   def support_notification(sender)
     @sender = sender
     mail(:to => sender.email,
-      :bcc => "contact@blackdogproperty.com",
+      :bcc => "enquiries@blackdogproperty.com",
       :from => sender.email,
       :subject => "Contact BlackDog Property")
   end
