b29753a38c4613f0938dc4b2b1d575c09bafe76b
Daren Serra Dias Warburton <daren.sdw@gmail.com>
2012-05-03 14:31:25 +0100

    updated photo order no check when no photos exist

--- app/controllers/admin/photos_controller.rb
+++ app/controllers/admin/photos_controller.rb
@@ -36,7 +36,11 @@ module Admin
       
       def next_order_no
         p = Photo.where(property_id: params[:property_id]).order_by(:order_no, :asc).excludes(order_no: 0).last
-        @order_no = p.order_no + 1
+        if p.present?
+          @order_no = p.order_no + 1
+        else
+          @order_no = 1
+        end
       end
   end
 end
